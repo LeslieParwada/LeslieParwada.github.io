df54dda14bbcb83ac4dfc49e003a29e945ce7c48
Leslie Parwada <62548096+LeslieParwada@users.noreply.github.com>
2020-03-24 21:01:02 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-//https://www.educative.io/edpresso/how-to-crop-an-image-in-css
-
-// https://gridbyexample.com/examples/
-
-// https://www.w3schools.com/css/css_grid.asp
-
-//https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Basic_Concepts_of_Grid_Layout
-
-// https://codepen.io/debu2code/pen/gGdMXp
-
-// https://codepen.io/freeCodeCamp/pen/zNqgVx
-
-// https://codepen.io/cod3rguy/pen/LGqmvV
-
-// https://codepen.io/JayV30/pen/XXONyK
-
-// https://codepen.io/ShaneSF/pen/OJPzwxM
-
-// https://static.clubs.nfl.com/image/private/t_new_photo_album/f_auto/ravens/cd9xdfcc3dz9gesd2nt9.jpg
-
 // !! IMPORTANT README:
 
 // You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
